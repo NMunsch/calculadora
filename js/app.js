c225a097a023afc160ad59cd2e78cb0227daee0b
manana <manana>
2016-10-21 13:55:09 +0200

    logica calculadora

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
 let coma = 0;
 let error = 0;
+let resultado = '';
+//let calculadora = new Calculadora();
 function refrescar() {
     document.getElementById("pantalla").value = "";
     localStorage.clear();
-    a = 0;
-    b = 0;
-    c = 0;
+    primerNumero = 0;
+    segundoNumero = 0;
+    operacion = 0;
     coma = 0;
     error = 0;
+    resultado = '';
 }
 
-function mostrar(a) {
+function mostrar(primerNumero) {
     if (error == 1) {
         document.getElementById("pantalla").value = "";
         error = 0;
     }
-    document.getElementById("pantalla").value = document.getElementById("pantalla").value + a;
-    if (isNaN(a) == true) {
+    document.getElementById("pantalla").value = document.getElementById("pantalla").value + primerNumero;
+    if (isNaN(primerNumero) == true) {
         coma = coma + 1;
         if (coma > 1) {
             document.getElementById("pantalla").value = "";
@@ -27,12 +30,12 @@ function mostrar(a) {
     }
 }
 
-function operadores(b) {
+function operadores(segundoNumero) {
     coma = 0;
     numero1 = document.getElementById("pantalla").value;
     localStorage.setItem("numero1", "pantalla");
     document.getElementById("pantalla").value = "";
-    document.getElementById("pantalla").value = document.getElementById("pantalla").value + b;
+    document.getElementById("pantalla").value = document.getElementById("pantalla").value + segundoNumero;
     operador = document.getElementById("pantalla").value;
     localStorage.setItem("operador", "pantalla");
     document.getElementById("pantalla").value = "";
@@ -42,15 +45,17 @@ function operar() {
     numero2 = document.getElementById("pantalla").value;
     localStorage.setItem("numero2", "pantalla");
     document.getElementById("pantalla").value = "";
-    let a = numero1;
-    let b = numero2;
-    let c = operador;
-    if (a == '') {
-        a = 0;
+    let primerNumero = numero1;
+    let segundoNumero = numero2;
+    let operacion = operador;
+    if (primerNumero == '') {
+        primerNumero = 0;
     }
-    if (b == '') {
-        b = 0;
+    if (segundoNumero == '') {
+        segundoNumero = 0;
     }
-    let resultado = eval(parseFloat(a) + c + parseFloat(b));
+    let resultado = eval(parseFloat(primerNumero) + operacion + parseFloat(segundoNumero));
+    // resultado = calculadora.operar(primerNumero,segundoNumero,operacion)
     document.getElementById("pantalla").value = document.getElementById("pantalla").value + resultado;
+    localStorage.clear;
 }
